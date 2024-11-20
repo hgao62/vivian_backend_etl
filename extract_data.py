145d86d6f29d37e8cbb2dabc7cc512b105b63503
github-xuanwang <35718382+github-xuanwang@users.noreply.github.com>
2024-11-20 01:05:07 -0500

    Added the second function get_stock_financials into extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -15,4 +15,25 @@ def get_stock_history(stock:str) -> pd.DataFrame:
 # Return the collected data
     return history_data
     
-print(get_stock_history("TSLA"))
+print(get_stock_history("MSFT"))
+
+
+def get_stock_financials(stock:str) -> pd.DataFrame:
+    '''this function should get share holders of a stock given a stock input,
+       please follow this link to get example on how to use yahoo finance api
+       https://github.com/ranaroussi/yfinance
+    '''
+    # Initialize the Ticker object for the specified
+    ticker = yf.Ticker(stock)
+    # get all stock info
+    stock_info = ticker.info
+    # get historical financial data
+    financials:pd.DataFrame = ticker.financials
+    transposed_financials = financials.transpose()
+
+# Return the collected data
+    return transposed_financials
+    
+print(get_stock_financials("MSFT"))
+
+
